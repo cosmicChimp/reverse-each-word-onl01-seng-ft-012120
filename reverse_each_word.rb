196a292bcc12d5208f78ac963930f93c59010238
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-23 21:52:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,8 @@
-def reverse_each_word
+def reverse_each_word_with_each(array)
+  first_array = array.split(" ")
+  new_array = []
+  first_array.each do |first|
+    new_array << first.reverse
+  end
+  new_array.join(" ")
+end
